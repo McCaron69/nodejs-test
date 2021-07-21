770b1af000b6d4f6d20ad4db860415cbd67fbb71
McCaron69 <artur.ivuskin172@gmail.com>
2021-07-22 02:13:54 +0300

    added new path and file using fs package; modified fs.js file

--- demo/fs.js
+++ demo/fs.js
@@ -1,2 +1,29 @@
 // Filesystem
 const fs = require('fs')
+const path = require('path')
+
+// fs.mkdir(path.join(__dirname, 'test'), (err) => {
+//     if (err) {
+//         throw err
+//     }
+
+//     console.log('Папка создана')
+// })
+
+const filePath = path.join(__dirname, 'test', 'text.txt')
+
+// fs.writeFile(filePath, 'Hello NodeJS!', err => {
+//     if (err) {
+//         throw err
+//     }
+
+//     console.log('Файл создан')
+
+//     fs.appendFile(filePath, '\nHello World!', err => {
+//         if (err) {
+//             throw err
+//         }
+
+//         console.log('В файл добавлена строчка')
+//     })    
+// })
