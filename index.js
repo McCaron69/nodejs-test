6e0137a1a274bdafce0eeba6cbe965469ff8bee3
McCaron69 <artur.ivuskin172@gmail.com>
2021-07-22 02:51:52 +0300

    added two pages in public folder; added code in index.js in root folder, to view them

--- index.js
+++ index.js
@@ -6,9 +6,36 @@
 // console.log(__dirname + " : " + __filename + " : " + require)
 
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
-    res.end('<h1>Hello NodeJS!!!</h1>')
+
+    if(req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+            if(err) {
+                throw err
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+
+            res.end(data)
+        })
+    } else if (req.url === '/contact') {
+        fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+            if(err) {
+                throw err
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+
+            res.end(data)
+        })
+    }
 })
 
 server.listen(3000, () => {
