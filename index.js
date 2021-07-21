b88031b889838035425a6d05453de8a65ed90b46
McCaron69 <artur.ivuskin172@gmail.com>
2021-07-22 02:41:11 +0300

    added start and dev scripts to package.json

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const chalk = require('chalk')
-const text = require('./data')
+// const chalk = require('chalk')
+// const text = require('./data')
 
-console.log(chalk.blue(text))
+// console.log(chalk.blue(text))
 
-console.log(__dirname + " : " + __filename + " : " + require)
+// console.log(__dirname + " : " + __filename + " : " + require)
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.end('<h1>Hello NodeJS!!!</h1>')
+})
+
+server.listen(3000, () => {
+    console.log('Server has been started...')
+})
